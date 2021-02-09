8c7323c8635c88431b99f5d4cfb6a6b4be9a245d
serkansahin55 <ssahin55@outlook.com>
2021-02-09 12:14:43 -0500

    fake auth working

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AuthNav from "./auth-nav"
 
 function NavBar() {
+
+  // const AuthNav = () => {
+  //   const { isAuthenticated } = useAuth0()
+
+  //   return (
+  //     <Nav>
+  //       {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+  //     </Nav>
+  //   )
+  // }
   return (
     <nav>
         <h1>AlaCarte Restaurant</h1>
+        <AuthNav />
       <ul>
         <li> 
           <Link to='/'> Home </Link> 
