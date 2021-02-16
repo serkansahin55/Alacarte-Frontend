656403d4359e14470d095733bf694793208d66f5
Serkan Sahin <ssahin55@outlook.com>
2021-02-15 22:23:38 -0500

    map api

--- src/components/AboutUsContainer.js
+++ src/components/AboutUsContainer.js
@@ -1,7 +1,13 @@
-import React from "react"
 import Navbar from "./Navbar"
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function AboutUsContainer() {
+function AboutUsContainer({google}) {
+
+    const mapStyles = {
+        width: '100%',
+        height: '100%'
+      };
 
     return (
         <div>
@@ -9,8 +15,23 @@ function AboutUsContainer() {
             <h1>555-555-5555</h1>
             <h1>123 ABC Street, Sesame, USA</h1>
             <h2>Where Family and Good Friends Meet!</h2>
+            <Map
+                google={google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                {
+                    lat: 39.827454,
+                    lng: -75.250243
+                }
+                }
+            />
         </div>
     )
 }
 
-export default AboutUsContainer
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBLqeKV7BMSaJXvShCIzK9D1rGwU3--Zpw'
+})(AboutUsContainer);
+
+
